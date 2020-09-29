7648b0849f295192d71278f9f917222eed8e29bb
C <gaetan.col@hotmail.fr>
2020-09-29 21:34:23 +0200

    V1.2

--- Ex2/Bagages.c
+++ Ex2/Bagages.c
@@ -6,34 +6,45 @@ int main() {
 	float L;
 	float l;
 	float p;
-
-	scanf_s("Donner la longueur %f\n", &L);
-	scanf_s("Donner la largeur %f\n", &l);
-	scanf_s("Donner la profondeur %f \n", &p);
-
-	if (L > 151.f && L < 1.f) {
-		printf("La longueur n'est pas valide, elle doit être comprise entre 1 et 150cm\n Donner la longueur\n");
-		scanf_s("Donner la longueur %f\n", &L);
+	float valeur1;
+	float valeur2;
+	float valeur3;
+	printf("Donnez la largeur du bagage\n");
+	scanf_s("%f", &L);
+	printf("Donnez la largeur du bagage\n");
+	scanf_s("%f", &l);
+	printf("Donnez la profondeur du bagage\n");
+	scanf_s("%f", &p);
+	if (p > 25) {
+		valeur1 = p;
+		if (valeur1 > 35) {
+			valeur2 = valeur1;
+			if (valeur2 > 55) {
+				printf("Les valeurs ne correspondent pas aux dimensions autorisées");
+			}
+			else {
+				L = valeur2;
+			}
+		}
+		else {
+			l = valeur1;
+		}
 	}
-	if (l > 151.f && l < 1.f) {
-		printf("La largeur n'est pas valide, elle doit être comprise entre 1 et 150cm\n Donner la largeur\n");
-		scanf_s("Donner la largeur %f\n", &l);
+	else {
+		if (l > 35) {
+			valeur1 = l;
+			if (valeur1 > 55) {
+				printf("Les valeurs ne correspondent pas aux dimensions autorisées");
+			}
+			else {
+				L = valeur1;
+			}
+		}
+		else {
+			if (L > 55) {
+				printf("Les valeurs ne correspondent pas aux dimensions autorisées");
+			}
+		}
 	}
-	if (p > 151.f && p < 1.f) {
-		printf("La profondeur n'est pas valide, elle doit être comprise entre 1 et 150cm\n Donner la profondeur\n");
-		scanf_s("Donner la profondeur %f \n", &p);
-	}
-	if (L > 55.f) {
-		printf("La longueur n'est pas valide, elle doit être inférieur à 55cm\n Donner la longueur\n");
-		scanf_s("Donner la longueur %f\n", &L);
-	}
-	if (l > 35.f) {
-		printf("La longueur n'est pas valide, elle doit être inférieur à 35cm\n Donner la longueur\n");
-		scanf_s("Donner la longueur %f\n", &L);
-	}
-	if (p > 25.f) {
-		printf("La longueur n'est pas valide, elle doit être inférieur à 25cm\n Donner la longueur\n");
-		scanf_s("Donner la longueur %f\n", &L);
-	}
-
+	printf("Votre bagage est éligible à l'embarquement");
 }
